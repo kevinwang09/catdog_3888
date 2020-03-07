185ac7904bd1445df6b5d5c965003cfe59ceaf26
Kevin Wang <kevin.wang@sydney.edu.au>
2020-03-07 16:12:16 -0500

    download pic based on URL

--- server.R
+++ server.R
@@ -1,5 +1,10 @@
 shinyServer(function(input, output) {
-    output$pred_table = renderDataTable(
-        pred_cats_dogs(model = model, file_name = "tmp.jpg")
-    )
+    output$pred_table = renderDataTable({
+        file_name = paste0(getwd(), "tmp.jpg")
+        
+        download.file(input$url,
+                      destfile = file_name)
+        
+        pred_cats_dogs(model = model, file_name = file_name)   
+    })
 })
